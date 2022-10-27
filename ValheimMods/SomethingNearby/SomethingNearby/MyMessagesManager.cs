16d7d5f32f388399555d9bc1443ffa73426e6b06
Noah Petrides <nopetrides@gmail.com>
2022-10-26 18:15:37 -0700

    First mod :)

---
+++ ValheimMods/SomethingNearby/SomethingNearby/MyMessagesManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomethingNearby
+{
+    /// <summary>
+    /// Script that manages the message queue
+    /// </summary>
+    internal class MyMessagesManager
+    {
+        private Queue<string> queuedMessages = new Queue<string>();
+
+        public void QueueMessage(string message)
+        {
+            if (SomethingNearby.Instance.ShowOnScreenMessages)
+            {
+                queuedMessages.Enqueue(message);
+                if (!SomethingNearby.Instance.IsScreenMessageShowing)
+                {
+                    SomethingNearby.Instance.ShowNextQueuedMessage();
+                }
+            }
+        }
+
+        public string GetNextQueuedMessage()
+        {
+            if (queuedMessages.Count > 0)
+            {   
+                return queuedMessages.Dequeue();
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+
+    }
+}
